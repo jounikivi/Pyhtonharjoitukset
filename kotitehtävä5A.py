3d11379519da6c98fe9761db17025646971b5b0d
jounikivi <jounikivipera@gmail.com>
2022-09-27 21:37:25 +0300

    Create kotitehtävä5A.py

---
+++ kotitehtävä5A.py
@@ -0,0 +1,7 @@
+#Kohdan 3.8 tehtävä 5 a
+#Assume you have the assignment xs = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+#a.Write a loop that prints each of the numbers on a new line.
+
+xs = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+for x in xs:
+    print(x)
